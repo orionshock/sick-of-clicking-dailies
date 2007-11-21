87569fb339948b016a064104982dede06d55f995
orionshock <orionshock@gmail.com>
2007-11-21 22:05:46 +0000

    -Add More 2.3 stuff and cross update from v5

--- core.lua
+++ core.lua
@@ -1,5 +1,5 @@
 ﻿--[[
-Major 4,  MinorSVN:  $Revision$
+Major 4,  MinorSVN:  Revision: 134 
 
 Sick Of Clicking Dailys is a simple addon designed to pick up and turn in Dailiy Quests for WoW.
 it does no checking to see if you have actualy completed them. If you have DailyFu installed it will quiry it for what
@@ -34,89 +34,162 @@ Distibuted under the "Do What The Fuck You Want To Public License" (http://sam.z
 =====================================================================================================
 ]]--
 SOCD = {}
-local mod = SOCD
+SOCD.version = 4 
+local addon = SOCD
 local L = SOCD_LOCALE_TABLE
 local DEF = DEFAULT_CHAT_FRAME --DEF:AddMessage("")
-mod["QuestTable"] = {   --<<<<<<<<<<<<============== THIS IS THE TABLE !!!!!
-	--Skettis Dailies 			>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	[L["Sky Sergeant Doryn"]] = {
-		enabled = true,
-		[L["Fires Over Skettis"]] = true,
-		[L["Escape from Skettis"]] = true,
-		},
-	[L["Skyguard Prisoner"]] = {
-		enabled = true,
-		[L["Escape from Skettis"]] = true,
-		},
-	--Blade's Edge Mountains	>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	[L["Skyguard Khatie"]] = {
-		enabled = true,
-		[L["Wrangle More Aether Rays!"]] = true,
-		},
-	[L["Sky Sergeant Vanderlip"]] = {
-		enabled = true,
-		[L["Bomb Them Again!"]] = true,
-		},
-	[L["Chu'a'lor"]] = {
-		enabled = true,
-		[L["The Relic's Emanation"]] = true,
-		},
-	[L["Kronk"]] = {
-		enabled = true,
-		[L["Banish More Demons"]] = true,
-		},
-	--Netherwing			>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	[L["Mistress of the Mines"]] = {
-		enabled = true,
-		[L["Picking Up The Pieces..."]] = true,
-		},
-	[L["Dragonmaw Foreman"]] = {
-		enabled = true,
-		[L["Dragons are the Least of Our Problems"]] = true,
-		},
-	[L["Yarzill the Merc"]] = {
-		enabled = true,
-		[L["The Not-So-Friendly Skies..."]] = true,
-		[L["A Slow Death"]] = false,
-		},
-	[L["Taskmaster Varkule Dragonbreath"]] = {
-		enabled = true,
-		[L["Nethercite Ore"]] = false,
-		[L["Netherdust Pollen"]] = false,
-		[L["Nethermine Flayer Hide"]] = true,
-		[L["Netherwing Crystals"]] = true,
-		},
-	[L["Chief Overseer Mudlump"]] = {
-		enabled = true,
-		[L["The Booterang: A Cure For The Common Worthless Peon"]] = true,
-		},
-	[L["Overlord Mor'ghor"]] = {
-		enabled = true,
-		[L["Disrupting the Twilight Portal"]] = true,
-		[L["The Deadliest Trap Ever Laid"]] = true,
-		},
-	[L["Commander Hobb"]] = { -- Scryer
-		enabled = false,
-		[L["The Deadliest Trap Ever Laid"]] = true,
-		},
-	[L["Commander Arcus"]] = { --Aldor
-		enabled = false,
-		[L["The Deadliest Trap Ever Laid"]] = true,
-		},
-	--Wintersaber Trainer
-	[L["Rivern Frostwind"]] = {
-		enabled = true,
-		[L["Frostsaber Provisions"]] = true,
-		[L["Winterfall Intrusion"]] = true,
-		[L["Rampaging Giants"]] = true,
-		}
-	}
-local MTable = mod["QuestTable"]
+addon["QuestTable"] = {
+				--Skettis Dailies
+				[L["Sky Sergeant Doryn"]] = { 
+					enabled = true,
+					[L["Fires Over Skettis"]] = true,
+					[L["Escape from Skettis"]] = true,
+					qOptions = {
+						[L["Escape from Skettis"]] = 3,
+						}
+					},
+				[L["Skyguard Prisoner"]] = {
+					enabled = true,
+					[L["Escape from Skettis"]] = true,
+					},
+				--Blade's Edge Mountains
+				[L["Skyguard Khatie"]] = {
+					enabled = true,
+					[L["Wrangle More Aether Rays!"]] = true,
+					},
+				[L["Sky Sergeant Vanderlip"]] = {
+					enabled = true,
+					[L["Bomb Them Again!"]] = true,
+					},
+				[L["Chu'a'lor"]] = {
+					enabled = true,
+					[L["The Relic's Emanation"]] = true,
+					},
+				[L["Kronk"]] = {
+					enabled = true,
+					[L["Banish More Demons"]] = true,
+					},
+				--Netherwing
+				[L["Mistress of the Mines"]] = {
+					enabled = true,
+					[L["Picking Up The Pieces..."]] = true,
+					},
+				[L["Dragonmaw Foreman"]] = {
+					enabled = true,
+					[L["Dragons are the Least of Our Problems"]] = true,
+					},
+				[L["Yarzill the Merc"]] = {
+					enabled = true,
+					[L["The Not-So-Friendly Skies..."]] = true,
+					[L["A Slow Death"]] = false,
+					},
+				[L["Taskmaster Varkule Dragonbreath"]] = {
+					enabled = true,
+					[L["Nethercite Ore"]] = false,
+					[L["Netherdust Pollen"]] = false,
+					[L["Nethermine Flayer Hide"]] = true,
+					[L["Netherwing Crystals"]] = true,
+					},
+				[L["Chief Overseer Mudlump"]] = {
+					enabled = true,
+					[L["The Booterang: A Cure For The Common Worthless Peon"]] = true,
+					},
+				[L["Overlord Mor'ghor"]] = {
+					enabled = true,
+					[L["Disrupting the Twilight Portal"]] = true,
+					[L["The Deadliest Trap Ever Laid"]] = false,	--Step 6 turn quest In.
+					},
+				[L["Commander Arcus"]] = {							--AldorNPC
+					enabled = true,
+					[L["Commander Arcus"]] = false,					--Step4 , Turn quest In; Aldor
+					[L["The Deadliest Trap Ever Laid"]] = false,	--Step 5, Pick up and Do Quest ; Aldor
+					},
+				[L["Commander Hobb"]] = {							-- ScryerNPC
+					enabled = true,
+					[L["Commander Hobb"]] = false,					--Step4 , Turn quest In; Scryer
+					[L["The Deadliest Trap Ever Laid"]] = false,	--Step 5, Pick up and Do Quest; Scryer
+					},
+			--Wintersaber Trainer
+				[L["Rivern Frostwind"]] = {
+					enabled = true,
+					[L["Frostsaber Provisions"]] = true,
+					[L["Winterfall Intrusion"]] = true,
+					[L["Rampaging Giants"]] = true,
+					},
+			--Cooking
+				[L["The Rokk"]] = {
+					enabled = true,
+					[L["Super Hot Stew"]] = true,
+					[L["Soup for the Soul"]] = true,
+					[L["Revenge is Tasty"]] = true,
+					[L["Manalicious"]] = true,
+					qOptions = {
+						[L["Super Hot Stew"]] = 3,
+						[L["Soup for the Soul"]] = 3,
+						[L["Revenge is Tasty"]] = 3,
+						[L["Manalicious"]] = 3,
+					}
+					},
+			--Instance
+				[L["Nether-Stalker Mah'duun"]] = {		---Non Heroic NPC
+					enabled = true,
+					[L["Wanted: Arcatraz Sentinels"]] = false,
+					[L["Wanted: Coilfang Myrmidons"]] = false,
+					[L["Wanted: Malicious Instructors"]] = false,
+					[L["Wanted: Rift Lords"]] = false,
+					[L["Wanted: Shattered Hand Centurions"]] = false,
+					[L["Wanted: Sunseeker Channelers"]] = false,
+					[L["Wanted: Tempest-Forge Destroyers"]] = false,
+				},
+				[L["Wind Trader Zhareem"]] = {			--Heroic Dailies Daily
+					enabled = true,
+					[L["Wanted: A Black Stalker Egg"]] = false,
+					[L["Wanted: A Warp Splinter Clipping"]] = false,
+					[L["Wanted: Aeonus's Hourglass"]] = false,
+					[L["Wanted: Bladefist's Seal"]] = false,
+					[L["Wanted: Keli'dan's Feathered Stave"]] = false,
+					[L["Wanted: Murmur's Whisper"]] = false,
+					[L["Wanted: Nazan's Riding Crop"]] = false,
+					[L["Wanted: Pathaleon's Projector"]] = false,
+					[L["Wanted: Shaffar's Wondrous Pendant"]] = false,
+					[L["Wanted: The Epoch Hunter's Head"]] = false,
+					[L["Wanted: The Exarch's Soul Gem"]] = false,
+					[L["Wanted: The Headfeathers of Ikiss"]] = false,
+					[L["Wanted: The Heart of Quagmirran"]] = false,
+					[L["Wanted: The Scroll of Skyriss"]] = false,
+					[L["Wanted: The Warlord's Treatise"]] = false,
+				},
+			--PvP
+				[L["Horde Warbringer"]] = {				--Horde AV Daily
+					enabled = true,
+					[L["Call to Arms: Alterac Valley"]] = true,
+					[L["Call to Arms: Arathi Basin"]] = true,
+					[L["Call to Arms: Eye of the Storm"]] = true,
+					[L["Call to Arms: Warsong Gulch"]] = true,
+				},
+				[L["Alliance Brigadier General"]] = {
+					enabled = true,
+					[L["Call to Arms: Alterac Valley"]] = true,
+					[L["Call to Arms: Arathi Basin"]] = true,
+					[L["Call to Arms: Eye of the Storm"]] = true,
+					[L["Call to Arms: Warsong Gulch"]] = true,
+				},
+				[L["Warrant Officer Tracy Proudwell"]] = {
+					enabled = true,
+					[L["Hellfire Fortifications"]] = true,
+				},
+				[L["Battlecryer Blackeye"]] = {
+					enabled = true,
+					[L["Hellfire Fortifications"]] = true,
+				},
+			}
 
-function mod:GOSSIP_SHOW()
+local MTable = addon["QuestTable"]
+
+function addon:GOSSIP_SHOW()
 	if IsShiftKeyDown() then return end
-	local npc = mod.CheckNPC()
-	local sel, quest, status = mod.OpeningCheckQuest(npc)
+	local npc = addon.CheckNPC()
+	local sel, quest, status = addon.OpeningCheckQuest(npc)
     if npc and quest then
 		if status == "Available" then
 			SelectGossipAvailableQuest(sel)
@@ -126,10 +199,10 @@ function mod:GOSSIP_SHOW()
     end
 end
 
-function mod:QUEST_DETAIL()
+function addon:QUEST_DETAIL()
 	if IsShiftKeyDown() then return end
-	local npc = mod.CheckNPC()
-	local quest = mod.TitleCheck(npc)
+	local npc = addon.CheckNPC()
+	local quest = addon.TitleCheck(npc)
     if npc and quest then
 		AcceptQuest()
     end
@@ -137,14 +210,14 @@ end
 
 local nextQuestFlag, questIndex = false, 0
 
-function mod:QUEST_PROGRESS()
+function addon:QUEST_PROGRESS()
     if IsShiftKeyDown() then return end
-	local npc = mod.CheckNPC()
-	local quest = mod.TitleCheck(npc)
+	local npc = addon.CheckNPC()
+	local quest = addon.TitleCheck(npc)
 	if npc and quest then
 		if not IsQuestCompletable() then
 			nextQuestFlag = true
-			DeclineQuest()
+			if self.db.profile.questLoop then DeclineQuest() end
 			return
 		else
 			nextQuestFlag = false
@@ -153,33 +226,30 @@ function mod:QUEST_PROGRESS()
     end
 end
 
-function mod:QUEST_COMPLETE()
+function addon:QUEST_COMPLETE()
 	nextQuestFlag = false
 	if IsShiftKeyDown() then return end
-	local npc = mod.CheckNPC()
-	local quest = mod.TitleCheck(npc)
+	local npc = addon.CheckNPC()
+	local quest = addon.TitleCheck(npc)
 	if npc and quest then
-		if quest == L["Escape from Skettis"] then
-			if DailyFu then
-				GetQuestReward(DailyFu:GetEscortOption())
-				return DailyFu:AddQuestToCompleted(quest)
-			end
-			return
-        else
-            if DailyFu then DailyFu:AddQuestToCompleted(quest) end
-			return GetQuestReward(0)
+		if MTable[npc].qOptions and  MTable[npc].qOptions[quest] then
+			local opt = self:GetQuestOption( nil, npc, quest)
+			if opt == 3 then return end
+			GetQuestReward( opt )
 		end
+
+		return GetQuestReward(0)
     end
-	
+
 end
 
-function mod.CheckNPC()
+function addon.CheckNPC()
 	local target = UnitName("target")
 	if MTable[target] then
 		if MTable[target].enabled then
 			return target
 		end
-	end	
+	end
 end
 
 local ghettoTable = { 1, 4, 7, 10, 13 } --Ghetto Table is the string # for the ... values
@@ -190,14 +260,14 @@ local function QuestItterate(npc, ...)
 	if nextQuestFlag then
 		nextQuestFlag = false
 		questIndex = questIndex + 1
-		if questIndex > (select( "#", ...)/3) then 
-			questIndex = 1 
+		if questIndex > (select( "#", ...)/3) then
+			questIndex = 1
 		end
-		
+
 		if npcTbl[select( ghettoTable[questIndex] , ... ) ] then
 			return questIndex, select( ghettoTable[questIndex] , ... )
 		end
-		
+
 	end
 	local qi = 0
 	for i=1, select("#", ...), 3 do
@@ -209,9 +279,9 @@ local function QuestItterate(npc, ...)
 	end
 end
 
-function mod.OpeningCheckQuest(npc)
+function addon.OpeningCheckQuest(npc)
 	if npc == nil then return end
-	local selection, quest = QuestItterate(npc, GetGossipAvailableQuests())	
+	local selection, quest = QuestItterate(npc, GetGossipAvailableQuests())
 	if quest then
 			return selection, quest, "Available"
 	else
@@ -222,38 +292,31 @@ function mod.OpeningCheckQuest(npc)
 	end
 end
 
---[[
-1Quest, 1lvl, 1nil, 2Quest, 2lvl, 2nil, 3Quest, 3lvl, 3nil, 4Quest, 4lvl, 4nil, 5Quest, 5lvl, 5nil,
-
-quest / selection
-
-1  1
-2 4 /2 = 2
-3 7 /2 = 
-4 8
-
- "A Slow Death", 70, nil,                                 1  , 2, 3
- "The Not-So-Friendly Skies...", 70, nil,       4. 5. 6
- "Accepting All Eggs", 70, nil                         7,  8, 9
- x, x, x,  						10, 11, 12
- yyy							13, 14, 15
-
-
-]]--
-function mod.TitleCheck(npc)
+function addon.TitleCheck(npc)
 	if npc == nil then return end
-	if MTable[npc][GetTitleText()] then 
+	if MTable[npc][GetTitleText()] then
 		return GetTitleText()
 	end
 end
 
-mod.eventFrame = CreateFrame("Frame", "SOCD_EVENT_FRAME", UIParent)
-mod.eventFrame:SetScript("OnEvent", function(self, event)
-		if mod[event] then
-			return mod[event]()
+function addon:GetQuestOption(info, eNpc, eQuest)
+	local npc, quest
+	if info then 
+		npc = info.arg[1]
+		quest = info.arg[2]
+	else
+		npc, quest = eNpc, eQuest
+	end
+	return MTable[npc].qOptions[quest]
+end
+
+addon.eventFrame = CreateFrame("Frame", "SOCD_EVENT_FRAME", UIParent)
+addon.eventFrame:SetScript("OnEvent", function(self, event)
+		if addon[event] then
+			return addon[event]()
 		end
     end)
-mod.eventFrame:RegisterEvent("GOSSIP_SHOW")
-mod.eventFrame:RegisterEvent("QUEST_DETAIL")
-mod.eventFrame:RegisterEvent("QUEST_PROGRESS")
-mod.eventFrame:RegisterEvent("QUEST_COMPLETE")
+addon.eventFrame:RegisterEvent("GOSSIP_SHOW")
+addon.eventFrame:RegisterEvent("QUEST_DETAIL")
+addon.eventFrame:RegisterEvent("QUEST_PROGRESS")
+addon.eventFrame:RegisterEvent("QUEST_COMPLETE")
--- localization-enUS.lua
+++ localization-enUS.lua
@@ -1,59 +1,164 @@
---Major 4, Minor $Revision$
+--Major 4, Minor Revision: 134 
 
-if GetLocale() == "enUS" or "enGB" then
-SOCD_LOCALE_TABLE = {
---QNPC's
-	--Skettis
-		["Sky Sergeant Doryn"] = true,
-		["Skyguard Prisoner"] = true, 
-		["Escape from Skettis"] = true,
-	--Blades Edge Mountains
-		["Skyguard Khatie"] = true,
-		["Sky Sergeant Vanderlip"] = true,
-		["Chu'a'lor"] = true,	
-		["Kronk"] = true,
-	--Netherwing
-		["Mistress of the Mines"] = true,
-		["Dragonmaw Foreman"] = true,
-		["Yarzill the Merc"] = true,
-		["Taskmaster Varkule Dragonbreath"] = true,
-		["Chief Overseer Mudlump"] = true,
-		["Overlord Mor'ghor"] = true,
-		["Commander Hobb"] = true, -- Scryer
-		["Commander Arcus"] = true, --Aldor
-	--Wintersaber Trainer
-		["Rivern Frostwind"] = true,
---Quests
-	--Skettis
-		["Fires Over Skettis"] = true,
-		["Escape from Skettis"] = true,
-	--Blades Edge Mountains
-		["Wrangle More Aether Rays!"] = true,
-		["Bomb Them Again!"] = true,
-		["The Relic's Emanation"] = true,
-		["Banish More Demons"] = true,
-	--Netherwing
-		["Picking Up The Pieces..."] = true,
-		["Dragons are the Least of Our Problems"] = true,
-		["The Not-So-Friendly Skies..."] = true,
-		["A Slow Death"] = true,
-		["Nethercite Ore"] = true,
-		["Netherdust Pollen"] = true,
-		["Nethermine Flayer Hide"] = true,
-		["Netherwing Crystals"] = true,
-		["The Booterang: A Cure For The Common Worthless Peon"] = true,
-		["Disrupting the Twilight Portal"] = true,
-		["The Deadliest Trap Ever Laid"] = true,
-	--Wintersaber Trainer
-		["Frostsaber Provisions"] = true,
-		["Winterfall Intrusion"] = true,
-		["Rampaging Giants"] = true,
+--local L = LibStub("AceLocale-3.0"):NewLocale("SickOfClickingDailies", "enUS", true)
+local L = {}
+if GetLocale() == "enUS" then 
+SOCD_LOCALE_TABLE = L
 
-	}
+--if L then
 
-	for k, v in pairs(SOCD_LOCALE_TABLE) do
-		if v == true then
-			SOCD_LOCALE_TABLE[k] = k
-		end
+	L["Sky Sergeant Doryn"] = true
+	L["Fires Over Skettis"] = true
+	L["Escape from Skettis"] = true
+
+	L["Skyguard Prisoner"] = true
+
+	L["Skyguard Khatie"] = true
+	L["Wrangle More Aether Rays!"] = true
+
+	L["Sky Sergeant Vanderlip"] = true
+	L["Bomb Them Again!"] = true
+
+	L["Chu'a'lor"] = true
+	L["The Relic's Emanation"] = true
+
+	L["Kronk"] = true
+	L["Banish More Demons"] = true
+
+	L["Mistress of the Mines"] = true
+	L["Picking Up The Pieces..."] = true
+
+	L["Dragonmaw Foreman"] = true
+	L["Dragons are the Least of Our Problems"] = true
+
+	L["Yarzill the Merc"] = true
+	L["The Not-So-Friendly Skies..."] = true
+	L["A Slow Death"] = true
+
+	L["Taskmaster Varkule Dragonbreath"] = true
+	L["Nethercite Ore"] = true
+	L["Netherdust Pollen"] = true
+	L["Nethermine Flayer Hide"] = true
+	L["Netherwing Crystals"] = true
+
+	L["Chief Overseer Mudlump"] = true
+	L["The Booterang: A Cure For The Common Worthless Peon"] = true
+
+	L["Overlord Mor'ghor"] = true
+	L["Disrupting the Twilight Portal"] = true
+	L["The Deadliest Trap Ever Laid"] = true
+
+	L["Commander Arcus"] = true
+	L["Commander Hobb"] = true
+
+	L["Rivern Frostwind"] = true
+	L["Frostsaber Provisions"] = true
+	L["Winterfall Intrusion"] = true
+	L["Rampaging Giants"] = true
+
+	L["The Rokk"] = true
+	L["Super Hot Stew"] = true
+	L["Soup for the Soul"] = true
+	L["Revenge is Tasty"] = true
+	L["Manalicious"] = true
+
+	L["Nether-Stalker Mah'duun"] = true
+	L["Wanted: Arcatraz Sentinels"] = true
+	L["Wanted: Coilfang Myrmidons"] = true
+	L["Wanted: Malicious Instructors"] = true
+	L["Wanted: Rift Lords"] = true
+	L["Wanted: Shattered Hand Centurions"] = true
+	L["Wanted: Sunseeker Channelers"] = true
+	L["Wanted: Tempest-Forge Destroyers"] = true
+
+	L["Wind Trader Zhareem"] = true
+	L["Wanted: A Black Stalker Egg"] = true
+	L["Wanted: A Warp Splinter Clipping"] = true
+	L["Wanted: Aeonus's Hourglass"] = true
+	L["Wanted: Bladefist's Seal"] = true
+	L["Wanted: Keli'dan's Feathered Stave"] = true
+	L["Wanted: Murmur's Whisper"] = true
+	L["Wanted: Nazan's Riding Crop"] = true
+	L["Wanted: Pathaleon's Projector"] = true
+	L["Wanted: Shaffar's Wondrous Pendant"] = true
+	L["Wanted: The Epoch Hunter's Head"] = true
+	L["Wanted: The Exarch's Soul Gem"] = true
+	L["Wanted: The Headfeathers of Ikiss"] = true
+	L["Wanted: The Heart of Quagmirran"] = true
+	L["Wanted: The Scroll of Skyriss"] = true
+	L["Wanted: The Warlord's Treatise"] = true
+
+	L["Horde Warbringer"] = true
+	L["Call to Arms: Alterac Valley"] = true
+	L["Call to Arms: Arathi Basin"] = true
+	L["Call to Arms: Eye of the Storm"] = true
+	L["Call to Arms: Warsong Gulch"] = true
+
+	L["Alliance Brigadier General"] = true
+	L["Warrant Officer Tracy Proudwell"] = true
+	L["Battlecryer Blackeye"] = true
+	L["Hellfire Fortifications"] = true
+
+-- Options Table Locale
+--General Titles
+	L["Sick Of Clicking Dailies?"] = true  ---- Addon Name used for Options table
+	L["NPC & Quest Options"] = true
+	L["NPC Enabled"] = true
+	L["General Options"] = true
+	L["Enabled"] = true
+	L["Always Loop NPCs"] = true
+	L["Always Loop on the NPC from one quest to the next forever"] = true
+	L["Enable Quest"] = true
+
+--Faction Groups
+	L["Faction Grinds"] = true
+		L["Skyguard"] = true
+		L["Ogri'la"] = true
+		L["Netherwing"] = true
+		L["Netherwing - Neutral"] = true
+		L["Netherwing - Friendly"] = true
+		L["Netherwing - Honored"] = true
+		L["Wintersaber Trainer"] = true
+	L["PvP Dailies"] = true
+		L["Horde PvP"] = true
+		L["Alliance PvP"] = true
+		L["Battlegrounds"] = true
+		L["World PvP"] = true
+
+	L["Instance Dailies"] = true
+		L["Normal Mode"] = true
+		L["Heroic Mode"] = true
+		
+		L["The Eye Heroic Dailies"] = true
+		L["Serpentshrine Cavern Heroic Dailies"] = true
+		L["Hellfire Citadel Heroic Dailies"] = true
+		L["Caverns of Time Heroic Dailies"] = true
+		L["Auchindoun Heroic Dailies"] = true
+	
+	L["Cooking Dailies"] = true
+
+--Special Tool Tips
+L["This will toggle the quest on both Doryn and the Prisoner"] = true
+L["|cffFF0000WARNING!!!|r, This Option also toggles both Scryer and Aldor Quests"] = true  ---Warning Color Code included in this string
+L["Currently All non-Heroic Quests are from\n |cff00ff00'Nether-Stalker Mah'duun'|r in lower city "] = true
+L["Heroic Mode Dailies available from: \n |cff00ff00'Wind Trader Zhareem' in LowerCity"] = true
+L["Currently All non-Heroic Quests are from\n |cff00ff00'Nether-Stalker Mah'duun'|r in lower city "] = true
+
+
+--Quest Options
+L["Quest Reward"] = true
+L["Select what Potion you want for the 'Escape from Skettis' quest"] = true
+L["None"] = true
+L["Health Potion"] = true
+L["Mana Potion"] = true
+L["Escort Quest Potion"] = true
+L["Barrel of Fish"] = true
+L["Crate of Meat"] = true
+
+for k,v in pairs(L) do
+	if v == true then
+		v = k
 	end
+end
+
 end
