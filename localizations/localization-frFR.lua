d7a8beafe9b5a6c265c213fa107b544df58327e4
Cinedelle <Cinedelle@dummy.example.com>
2008-12-09 19:48:46 +0000

    French local file updated with the latest alpha improvement on WotLK

--- localizations/localization-frFR.lua
+++ localizations/localization-frFR.lua
@@ -21,8 +21,9 @@ if L then
 	L["Quest Rewards"] = "Récompenses de quêtes"
 	L["Quests"] = QUESTS_LABEL
 	L["Wanted: "] = "On recherche : "		--Used in Instance quest for dsplay...
+	L["Horde"] = FACTION_HORDE
+	L["Alliance"] = FACTION_ALLIANCE
 	
-
 	--Classic Section
 	L["Classic WoW"] = "WoW Classique"
 	L["Wintersaber Trainers"] = "Éleveur de sabres-d'hiver"
@@ -52,6 +53,30 @@ if L then
 	L["Professions"] = TRADE_SKILLS
 	L["Cooking"] = "Cuisine"
 	L["Fishing"] = "Pêche"
+	
+		--LichKing
+	L["LK"] = "Lich King"
+		--Factions
+	L["The Wyrmrest Accord"] = "L'Accord de Repos du ver"
+	L["Sholazar Basin"] = "Bassin de Sholazar"
+	L["The Oracles"] = "Les Oracles"
+	L["Frenzyheart Tribe"] = "Tribu Frénécœur"
+	L["The Sons of Hodir"] = "Les Fils de Hodir"
+	L["Argent Crusade"] = "La Croisade d'argent"
+	L["Knights of the Ebon Blade"] = "Chevaliers de la Lame d'ébène"
+	L["The Kalu'ak"] = "Les Kalu'aks"
+	L["The Storm Peaks"] = "Les pics Foudroyés"
+
+		--Misc Titles
+	L["Shared Faction Quests"] = "Quêtes communes aux factions"
+	L["Icecrown"] = "La Couronne de glace"
+	L["Grizzly Hills"] = "Les Grisonnes"
+	L["Wintergrasp"] = "Joug-d'hiver"
+	L["Shared Quests"] = "Quêtes communes"
+
+	L["Troll Patrol: "] = "Patrouille anti-trolls : "	--There is a space at the end, this is used to clip ugly quests.
+	
+	
 end
 
 --All Quests Classified by orgin / content location
@@ -206,5 +231,187 @@ end
 local LK = LibStub("AceLocale-3.0"):NewLocale("SOCD_LK", "frFR", false)
 
 if LK then
-	LK["test"] = "test"
+	--Instance Dailies
+	LK["All Things in Good Time"] = "Tout vient à point…"
+		--Heroics
+	LK["Proof of Demise: Anub'arak"] = "Preuve de la mort d'Anub'arak"
+	LK["Proof of Demise: Cyanigosa"] = "Preuve de la mort de Cyanigosa"
+	LK["Proof of Demise: Gal'darah"] = "Preuve de la mort de Gal'darah"
+	LK["Proof of Demise: Herald Volazj"] = "Preuve de la mort du héraut Volazj"
+	LK["Proof of Demise: Ingvar the Plunderer"] = "Preuve de la mort d'Ingvar le Pilleur"
+	LK["Proof of Demise: Keristrasza"] = "Preuve de la mort de Keristrasza"
+	LK["Proof of Demise: King Ymiron"] = "Preuve de la mort du roi Ymiron"
+	LK["Proof of Demise: Ley-Guardian Eregos"] = "Preuve de la mort du gardien-tellurique Eregos"
+	LK["Proof of Demise: Loken"] = "Preuve de la mort de Loken"
+	LK["Proof of Demise: Mal'Ganis"] = "Preuve de la mort de Mal'Ganis"
+	LK["Proof of Demise: Sjonnir The Ironshaper"] = "Preuve de la mort de Sjonnir le Sculptefer"
+	LK["Proof of Demise: The Prophet Tharon'ja"] = "Preuve de la mort du prophète Theron'ja"
+		--Non Heroics
+	LK["Timear Foresees Centrifuge Constructs in your Future!"] = "Timear prédit des assemblages centrifuges dans votre avenir !"
+	LK["Timear Foresees Infinite Agents in your Future!"] = "Timear prédit des agents infinis dans votre avenir !"
+	LK["Timear Foresees Titanium Vanguards in your Future!"] = "Timear prédit des avant-gardes en titane dans votre avenir !"
+	LK["Timear Foresees Ymirjar Berserkers in your Future!"] = "Timear prédit des berserkers ymirjar dans votre avenir !"
+
+	--Professions
+		--Cooking
+	LK["Cheese for Glowergold"] = "Du fromage pour Froncelor"
+	LK["Convention at the Legerdemain"] = "Convention à l'Abracadabar"
+	LK["Infused Mushroom Meatloaf"] = "Pain de viande aux champignons infusés"
+	LK["Mustard Dogs!"] = "Hot-dogs à la moutarde !"
+	LK["Sewer Stew"] = "Le ragoût des égouts"
+		--Jewlcrafting
+	LK["Shipment: Blood Jade Amulet"] = "Livraison : Amulette en jade de sang"
+	LK["Shipment: Bright Armor Relic"] = "Livraison : Relique de l'armure éclatante"
+	LK["Shipment: Glowing Ivory Figurine"] = "Livraison : Figurine luminescente en ivoire"
+	LK["Shipment: Intricate Bone Figurine"] = "Livraison : Figurine complexe en os"
+	LK["Shipment: Shifting Sun Curio"] = "Livraison : Bibelot du soleil changeant"
+	LK["Shipment: Wicked Sun Brooch"] = "Livraison : Broche du soleil pernicieux"
+
+	--Factions
+		--The Wyrmrest Accord
+	LK["Aces High!"] = "Un as dans notre jeu !"
+	LK["Drake Hunt"] = "La chasse au drake"
+	LK["Defending Wyrmrest Temple"] = "La défense du temple du Repos du ver"
+
+		--The Oracles
+	LK["A Cleansing Song"] = "Un chant de purification"
+	LK["Appeasing the Great Rain Stone"] = "Un chant de purification"
+	LK["Hand of the Oracles"] = "La main des oracles"
+	LK["Mastery of the Crystals"] = "La maîtrise des cristaux"
+	LK["Power of the Great Ones"] = "Le pouvoir des Tout-puissants"
+	LK["Song of Fecundity"] = "Un chant de fécondité"
+	LK["Song of Reflection"] = "Un chant de réflexion"
+	LK["Song of Wind and Water"] = "Le chant du Vent et de l'Eau"
+	LK["Will of the Titans"] = "La volonté des titans"
+
+		--Frenzyheart Tribe
+	LK["A Hero's Headgear"] = "Un couvre-chef de héros"
+	LK["Chicken Party!"] = "Des poulets et des varleus"
+	LK["Frenzyheart Champion"] = "Le champion frénécœur"
+	LK["Kartak's Rampage"] = "Le massacre de Kartak"
+	LK["Rejek: First Blood"] = "Rejek : le premier sang"
+	LK["Secret Strength of the Frenzyheart"] = "La puissance secrète des Frénécœurs"
+	LK["Strength of the Tempest"] = "La puissance secrète des Frénécœurs"
+	LK["The Heartblood's Strength"] = "La force du sang du coeur"
+	LK["Tools of War"] = "De drôle d'outils de guerre"
+
+		--The Sons of Hodir
+	LK["Blowing Hodir's Horn"] = "Souffler dans le cor de Hodir"
+	LK["Feeding Arngrim"] = "Feeding Arngrim"
+	LK["Hot and Cold"] = "Chaud et froid"
+	LK["Polishing the Helm"] = "Polir le heaume"
+	LK["Spy Hunter"] = "À la chasse aux espions"
+	LK["Thrusting Hodir's Spear"] = "Planter la lance de Hodir"
+
+		--Argent Crusade
+	LK["The Alchemist's Apprentice"] = "L'apprenti alchimiste"
+	LK["Troll Patrol"] = "Patrouille anti-trolls"
+	LK["Troll Patrol: Can You Dig It?"] = "Patrouille anti-trolls : et que ça creuse !"
+	LK["Troll Patrol: Couldn't Care Less"] = "Patrouille anti-trolls : je m'en fiche totalement !"
+	LK["Troll Patrol: Creature Comforts"] = "Patrouille anti-trolls : une bonne flambée"
+	LK["Troll Patrol: Done to Death"] = "Patrouille anti-trolls : morts incinérés"
+	LK["Troll Patrol: High Standards"] = "Patrouille anti-trolls : hissez les étendards !"
+	LK["Troll Patrol: Intestinal Fortitude"] = "Patrouille anti-trolls : un peu de courage !"
+	LK["Troll Patrol: Something for the Pain"] = "Patrouille anti-trolls : un remède contre la douleur"
+	LK["Troll Patrol: The Alchemist's Apprentice"] = "Patrouille anti-trolls : l'apprenti alchimiste"
+	LK["Troll Patrol: Throwing Down"] = "Patrouille anti-trolls : attention en dessous !"
+	LK["Troll Patrol: Whatdya Want, a Medal?"] = "Patrouille anti-trolls : vous voulez quoi ? Une médaille ?"
+	LK["Congratulations!"] = "Félicitations !"
+
+		--Knights of the Ebon Blade
+	LK["Intelligence Gathering"] = "Trouver des informations"
+	LK["Leave Our Mark"] = "Laisser notre marque"
+	LK["No Fly Zone"] = "Zone d'exclusion aérienne"
+	LK["From Their Corpses, Rise!"] = "Réanimez leurs cadavres !"
+	LK["Shoot 'Em Up"] = "Descendez-les tous !"
+	LK["Vile Like Fire!"] = "Descendez-les tous !"
+
+		--The Kalu'ak
+	LK["Planning for the Future"] = "Préparer l'avenir"
+	LK["Preparing for the Worst"] = "Se préparer au pire"
+	LK["The Way to His Heart..."] = "Le chemin de son cœur…"
+
+		--The Frostborn
+	LK["Pushed Too Far"] = "Trop loin"
+
+	----Horde Expedition / --Alliance Vanguard
+		--These are shared quests for the given zone
+		--IceCrown
+	LK["King of the Mountain"] = "Roi de la montagne"	--Netural
+	LK["Blood of the Chosen"] = "Le sang des Élus"	--Netural
+	LK["Drag and Drop"] = "Attraper et lâcher"	--Netural
+	LK["Neutralizing the Plague"] = "Neutraliser la peste"	--Netural
+	LK["No Rest For The Wicked"] = "Pas de repos pour le malfaisant"	--Netural
+	LK["Not a Bug"] = "Il n'est pas nuisible"	--Netural
+	LK["Retest Now"] = "Testez-le à nouveau"	--Netural
+	LK["Slaves to Saronite"] = "Esclaves de la saronite"	--Netural
+	LK["That's Abominable!"] = "C'est abominable !"	--Netural
+	LK["Static Shock Troops: the Bombardment"] = "Troupe de choc statique : le Bombardement"	--Alliance
+	LK["Total Ohmage: The Valley of Lost Hope!"] = "Ohmage total : la vallée de l'Espoir perdu !"	--Horde
+	LK["The Solution Solution"] = "La solution pour la solution"	--Alliance
+	LK["Volatility"] = "Volatilité"	--Horde
+	LK["Capture More Dispatches"] = "Récupérez plus de dépêches"	--Alliance
+	LK["Keeping the Alliance Blind"] = "Que l'Alliance reste aveugle"	--Horde
+	LK["Putting the Hertz: The Valley of Lost Hope"] = "Objectif Killo : la vallée de l'Espoir perdu"	--Alliance
+	LK["Riding the Wavelength: The Bombardment"] = "Profiter de l'onde : le Bombardement"	--Horde
+
+		--Grizzly Hills
+	LK["Life or Death"] = "La vie ou la mort"	--Alliance
+	LK["Overwhelmed!"] = "Débordée !"	--Horde
+	LK["Making Repairs"] = "Procéder aux réparations"	--Horde
+	LK["Pieces Parts"] = "Pour quelques bouts de ferraille de plus…"	--Alliance
+
+		--The Storm Peaks
+	LK["Back to the Pit"] = "Retour dans la fosse"
+	LK["Defending Your Title"] = "Défendre votre titre"
+	LK["Overstock"] = "Surplus"
+	LK["Maintaining Discipline"] = "Maintenir la discipline"
+	LK["The Aberrations Must Die"] = "Les aberrations doivent mourir"
+
+
+		--World PvP
+		--These Quests Specificlally flag you as PvP Active
+		--Wintergrasp Fortress
+	LK["A Rare Herb"] = "Une herbe rare"
+	LK["Bones and Arrows"] = "Des os et des flèches"
+	LK["Defend the Siege"] = "La défense du siège"
+	LK["Fueling the Demolishers"] = "Alimenter les démolisseurs"
+	LK["Healing with Roses"] = "Guérison par les plantes"
+	LK["Jinxing the Walls"] = "Ensorceler les murs"
+	LK["No Mercy for the Merciless"] = "Pas de pitié pour les sans pitié"
+	LK["Slay them all"] = "Tuez-les tous !"
+	LK["Stop the Siege"] = "L'arrêt du siège"
+	LK["Victory in Wintergrasp"] = "Victoire à Joug-d'hiver"
+	LK["Warding the Walls"] = "Surveiller les murs"
+	LK["Warding the Warriors"] = "Protéger les guerriers"
+
+		--BattleGround
+	LK["Call to Arms: Strand of the Ancients"] = "Appel aux armes : rivage des Anciens"
+
+		--IceCrown
+	LK["Make Them Pay!"] = "Faites-les payer !"	--Horde
+	LK["Shred the Alliance"] = "Déchiquetez l'Alliance"	--Horde
+	LK["No Mercy!"] = "Pas de pitié !"	--Allaince
+	LK["Shredder Repair"] = "Réparation de déchiqueteurs"	--Alliance
+
+		--Grizzly Hills
+	LK["Keep Them at Bay"] = "Ra-baie-ssez l'ennemi"	--Netural
+	LK["Riding the Red Rocket"] = "À cheval sur la fusée…"	--Netural
+	LK["Seared Scourge"] = "Fléau flambé"	--Netural
+	LK["Smoke 'Em Out"] = "Enfumez-les"	--Netural
+
+	LK["Down With Captain Zorna!"] = "Mort au capitaine Zorna !"	--Alliance
+	LK["Kick 'Em While They're Down"] = "Frappez-les tant qu'ils sont à terre"	--Alliance
+	LK["Blackriver Skirmish"] = "Escarmouches sur la rivière Noire"	--Alliance
+
+	LK["Crush Captain Brightwater!"] = "Abattez le capitaine Brilleflot !"	--Horde
+	LK["Keep 'Em on Their Heels"] = "Mettez-les sur les dents"	--Horde
+	LK["Blackriver Brawl"] = "Baston sur la rivière Noire"	--Horde
+
+
+	--Misc
+		--Howling Fjord
+	LK["Break the Blockade"] = "Forcer le blocus"	--Alliance
+	LK["Steel Gate Patrol"] = "Patrouille à la Porte d'acier"	--Alliance
+
 end
