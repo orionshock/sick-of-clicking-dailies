296ed1e174b7fd77cfba151e1956bceedfbc0c6e
orionshock <orionshock@gmail.com>
2009-02-01 23:19:15 +0000

    round 2, touchup enEN file to be more consistant.

--- localizations/localization-enUS.lua
+++ localizations/localization-enUS.lua
@@ -10,68 +10,69 @@ local debug = false
 debug = true
 --@end-debug@
 
---General Section
+----------------------------------------------------------------------------
+--	General -- Used by all or most :)				  --
+----------------------------------------------------------------------------
+
 local L = LibStub("AceLocale-3.0"):NewLocale("SOCD_Core", "enUS", true, debug)
 
-if not L then return end
+	if L then
+--These are not importable to WoWAce/Curseforge Locale system, as these are pulled directly from the client.
+L["Battlegrounds"] = BATTLEFIELDS
+L["Netural"] = FACTION_STANDING_LABEL4
+L["Friendly"] = FACTION_STANDING_LABEL5
+L["Honored"] = FACTION_STANDING_LABEL6
+L["Revered"] = FACTION_STANDING_LABEL7
+L["Faction"] = FACTION
+L["None"] = LFG_TYPE_NONE
+L["Quests"] = QUESTS_LABEL
+L["Horde"] = FACTION_HORDE
+L["Alliance"] = FACTION_ALLIANCE
+L["Professions"] = TRADE_SKILLS
+L["Cooking"] = (GetSpellInfo(2550))
+L["Fishing"] = (GetSpellInfo(7733))
+L["Jewelcrafting"] = (GetSpellInfo(25229))
 
-if L then
-	--These are not importable to WoWAce/Curseforge Locale system, as these are pulled directly from the client.
-	L["Battlegrounds"] = BATTLEFIELDS
-	L["Netural"] = FACTION_STANDING_LABEL4
-	L["Friendly"] = FACTION_STANDING_LABEL5
-	L["Honored"] = FACTION_STANDING_LABEL6
-	L["Revered"] = FACTION_STANDING_LABEL7
-	L["Faction"] = FACTION
-	L["None"] = LFG_TYPE_NONE
-	L["Quests"] = QUESTS_LABEL
-	L["Horde"] = FACTION_HORDE
-	L["Alliance"] = FACTION_ALLIANCE
-		--Professions
-	L["Professions"] = TRADE_SKILLS
-	L["Cooking"] = (GetSpellInfo(2550))
-	L["Fishing"] = (GetSpellInfo(7733))
-	L["Jewelcrafting"] = (GetSpellInfo(25229))
-	--
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Base_All", table-name="L")@
 
-	--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Base_All", table-name="L")@
-
-end	
+	end	
 
 --All Quests Classified by orgin / content location
 
---
---	Classic, Populated by WoWAce-Curseforge Packager.
---
+----------------------------------------------------------------------------
+--	Classic, Populated by WoWAce-Curseforge Packager.		  --
+----------------------------------------------------------------------------
 
 local C = LibStub("AceLocale-3.0"):NewLocale("SOCD_Classic", "enUS", true, debug)
 
-if C then
+	if C then
 
-	--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Classic", table-name="C")@
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Classic", table-name="C")@
 
-end
+	end
 
---
---	TBC, Populated by WoWAce-Curseforge Packager.
---	
+----------------------------------------------------------------------------
+--	TBC, Populated by WoWAce-Curseforge Packager.			  --
+----------------------------------------------------------------------------	
 
 local BC = LibStub("AceLocale-3.0"):NewLocale("SOCD_BC", "enUS", true, debug)
 
-if BC then
+	if BC then
 
-	--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="BC", table-name="BC")@
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="BC", table-name="BC")@
 
-end
+	end
 
---
---	WOTLK, Populated by WoWAce-Curseforge Packager.
---
+----------------------------------------------------------------------------
+--	WOTLK, Populated by WoWAce-Curseforge Packager.			  --
+----------------------------------------------------------------------------
 
 local LK = LibStub("AceLocale-3.0"):NewLocale("SOCD_LK", "enUS", true, debug)
 
-if LK then
+	if LK then
 
-	--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Wrath", table-name="LK")@
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Wrath", table-name="LK")@
 
-end
+	end
+
+---Localization Counter-- Bump to generate new zip for locale changes = = = 2
