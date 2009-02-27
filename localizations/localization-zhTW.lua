0fb3a70a963abe88c2c15713bd42d6f7d4de26ac
orionshock <orionshock@gmail.com>
2009-02-27 14:06:43 +0000

    add in zhTW locale.

---
+++ localizations/localization-zhTW.lua
@@ -0,0 +1,78 @@
+--[[
+	Sick Of Clicking Dailies? - Locale file for zhTW
+	Written By: @project-author@
+	
+	Please Visit: http://www.wowace.com/projects/sick-of-clicking-dailies/localization/zhTW
+	to contribute to localizations :)
+]]--
+
+
+
+
+
+
+----------------------------------------------------------------------------
+--	General -- Used by all or most :)				  --
+----------------------------------------------------------------------------
+
+local L = LibStub("AceLocale-3.0"):NewLocale("SOCD_Core", "zhTW", false)
+
+	if L then
+--These are not importable to WoWAce/Curseforge Locale system, as these are pulled directly from the client.
+L["Alliance"] = FACTION_ALLIANCE
+L["Battlegrounds"] = BATTLEFIELDS
+L["Cooking"] = (GetSpellInfo(2550))
+L["Faction"] = FACTION
+L["Fishing"] = (GetSpellInfo(7733))
+L["Friendly"] = FACTION_STANDING_LABEL5
+L["Honored"] = FACTION_STANDING_LABEL6
+L["Horde"] = FACTION_HORDE
+L["Jewelcrafting"] = (GetSpellInfo(25229))
+L["None"] = LFG_TYPE_NONE
+L["Netural"] = FACTION_STANDING_LABEL4
+L["Professions"] = TRADE_SKILLS
+L["PvP"] = PVP
+L["Quests"] = QUESTS_LABEL
+L["Revered"] = FACTION_STANDING_LABEL7
+
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, namespace="Base_All", table-name="L")@
+
+	end	
+
+--All Quests Classified by orgin / content location
+
+----------------------------------------------------------------------------
+--	Classic, Populated by WoWAce-Curseforge Packager.		  --
+----------------------------------------------------------------------------
+
+local C = LibStub("AceLocale-3.0"):NewLocale("SOCD_Classic", "zhTW", false)
+
+	if C then
+
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, namespace="Classic", table-name="C")@
+
+	end
+
+----------------------------------------------------------------------------
+--	TBC, Populated by WoWAce-Curseforge Packager.			  --
+----------------------------------------------------------------------------	
+
+local BC = LibStub("AceLocale-3.0"):NewLocale("SOCD_BC", "zhTW", false)
+
+	if BC then
+
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, namespace="BC", table-name="BC")@
+
+	end
+
+----------------------------------------------------------------------------
+--	WOTLK, Populated by WoWAce-Curseforge Packager.			  --
+----------------------------------------------------------------------------
+
+local LK = LibStub("AceLocale-3.0"):NewLocale("SOCD_LK", "zhTW", false)
+
+	if LK then
+
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, namespace="Wrath", table-name="LK")@
+
+	end
