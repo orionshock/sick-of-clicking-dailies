c6e5a7b954f8127d6dd61b7a9302d581382ad7e3
orionshock <orionshock@gmail.com>
2009-08-03 03:53:11 +0000

    remove the debug commenting from core file, that didn't work as i wanted it to.

--- core.lua
+++ core.lua
@@ -29,7 +29,6 @@ addon.Version = projectVersion..projectRevision
 --
 local D = function() end
 --@alpha@
---@end-alpha@
 function D(arg, ...)
 	local str
 	if string.find(tostring(arg), "%%") then
@@ -44,7 +43,6 @@ function D(arg, ...)
 	return str
 end
 --@end-alpha@
-
 
 --
 --	Addon Decleration & File Wide locals
@@ -112,9 +110,9 @@ function addon:UnRegisterQuests(name)
 		moduleQOptions[name] = nil
 		questNPCs[name] = nil
 		moduleGossipOptions[name] = nil
---@alpha@
+
 		D("Quest Grouping %s unregistered", name)
---@end-alpha@
+
 	end
 end
 
@@ -154,9 +152,9 @@ local function GetOptionsTable()
 				name = L["Module Control"],
 				type = "group", order = -1,
 				args = {
-					--@alpha@
+					
 					debug = { type = "toggle", name = "Enable Debug", get = function() return addon.db.profile.debug end, set = function(_, val) addon.db.profile.debug = val end },
-					--@end-alpha@
+					
 				},
 			},
 		},
@@ -243,72 +241,48 @@ local npcBad, nextQuestFlag, questIndex, gossipPop = nil, false, 1, false
 local stopFlag, s_title, s_npc = false	--Event Dispatching stuff..
 
 function addon:GOSSIP_SHOW(event)
---@alpha@
 	D(event)
---@end-alpha@
 	local stopFlag, s_title, s_npc = false, nil, nil
 	local npc = self:CheckNPC(event)
 	if not npc then
---@alpha@
 		D(event, "no known npc")
---@end-alpha@
 		return
 	end
 	if (IsShiftKeyDown()) then return end
 	local sel, quest, status = self:OpeningCheckQuest(event)
---@alpha@
 	D(event, "logic batterie sel:", sel, "quest:", quest, "status:", status)
---@end-alpha@
 	if sel then
---@alpha@
 		D(event, "We have quest selection", sel, quest, status, "| BadNPC:", npcBad)
---@end-alpha@
 		if npcBad then
---@alpha@
 			D(event, "badNPC, gossipDive")
---@end-alpha@
 			self:DoGossipOptions(event.."~EoQ" )
 			if not self.db.profile.questLoop then
---@alpha@
 				D(event,"No QuestLooping, exit Func")
---@end-alpha@
 				return
 			end
 		end
---@alpha@
 		D(event, npcBad and "QuestLoopingEnabled" )
 		D(event, "Interacting with NPC", status, sel, quest)
---@end-alpha@
 		if status == "Available" then
 			return SelectGossipAvailableQuest(sel)
 		elseif status == "Active" then
 			return SelectGossipActiveQuest(sel)
 		end
 	elseif not sel then
---@alpha@
 		D(event, "No quests?? just gossip")
---@end-alpha@
 		self:DoGossipOptions(event.."~NoQ")
 	end
 end
 
 function addon:DoGossipOptions(te)
---@alpha@
 	te = "GosOpt~"..te
 	D(te)
---@end-alpha@
 	local hasGossip, index = self:AnalyzeGossipOptions(te, GetGossipOptions() )
---@alpha@
 	D(te, hasGossip, index)
---@end-alpha@
 	if hasGossip then
---@alpha@
 		D(te, hasGossip:sub(1,8), index)
---@end-alpha@
 		npcBad =  false
---@alpha@
 		D(te, "selectingOpt", index)
---@end-alpha@
 		SelectGossipOption(index)
 	end
 end
@@ -317,15 +291,12 @@ function addon:QUEST_GREETING(event, ...)
 	local stopFlag, s_title, s_npc = false, nil, nil
 	local npc = self:CheckNPC(event)
 	if not npc then
---@alpha@
 		D(event, "no known npc")
---@end-alpha@
 		return
 	end
 	if (IsShiftKeyDown()) then return end
 	local numActiveQuests = GetNumActiveQuests();
 	local numAvailableQuests = GetNumAvailableQuests();
-
 	if numAvailableQuests > 0 then
 		local selection, quest = self:QuestItteratePickUp(event, GetAvailableTitle(1), GetAvailableTitle(2), GetAvailableTitle(3), GetAvailableTitle(4), GetAvailableTitle(5), GetAvailableTitle(6) )
 		if selection and quest then
@@ -346,19 +317,13 @@ function addon:QUEST_GREETING(event, ...)
 end
 
 function addon:QUEST_DETAIL(event)
---@alpha@
 	D(event)
---@end-alpha@
 	if IsShiftKeyDown() then return end
 	local npc = self:CheckNPC(event)
 	local quest = self:TitleCheck(event)
---@alpha@
 	D(event, "found:", npc, quest)
---@end-alpha@
 	if npc and quest then
---@alpha@
 		D(event,"Accepting Quest", quest, npc)
---@end-alpha@
 		AcceptQuest()
 		return
 	end
@@ -366,32 +331,22 @@ end
 
 
 function addon:QUEST_PROGRESS(event)
---@alpha@
 	D(event)
---@end-alpha@
    	if IsShiftKeyDown() then return end
 	local npc = self:CheckNPC(event)
 	local quest = self:TitleCheck(event)
---@alpha@
 	D(event, "found:", npc, quest)
---@end-alpha@
 	if npc and quest then
 		if not IsQuestCompletable() then
---@alpha@
 			D(event, "QuestNotCompleteable, set flag and DeclineQuest()")
---@end-alpha@
 			nextQuestFlag = true
 			DeclineQuest()
 			return
 		else
---@alpha@
 			D(event, "set nextQuestFlag to false")
---@end-alpha@
 			nextQuestFlag = false
 		end
---@alpha@
 		D(event, "Turning in quest")
---@end-alpha@
 		return CompleteQuest() --HERE
     end
 end
@@ -399,9 +354,7 @@ end
 do
 	function addon:QUEST_COMPLETE(event)
 		stopFlag = false
---@alpha@
 		D(event, "nextQuestFlag to false")
---@end-alpha@
 		nextQuestFlag = false
 		if IsShiftKeyDown() then return end
 		local npc = self:CheckNPC(event)
@@ -411,22 +364,16 @@ do
 			if (opt and (opt == 5)) then
 				stopFlag = true
 				s_title, s_npc = quest, npc
---@alpha@
 				D(event, "Has Option and time to stop", quest, opt)
---@end-alpha@
 				return
 			elseif opt and (opt >= 1 and opt <= 4 ) then
 				stopFlag = false
---@alpha@
 				D(event, "Getting Money!", opt)
---@end-alpha@
 				GetQuestReward( opt )
 				self:SendMessage("SOCD_DAILIY_QUEST_COMPLETE", quest, npc, opt)
 				return
 			end
---@alpha@
 			D(event, "Getting Money!")
---@end-alpha@
 			GetQuestReward(0)
 			self:SendMessage("SOCD_DAILIY_QUEST_COMPLETE", quest, npc)
 			return
@@ -450,24 +397,18 @@ do
 end
 
 function addon:PLAYER_TARGET_CHANGED(event)
---@alpha@
 	D(event, "Set nextQuestFlag to false")
---@end-alpha@
 	npcBad, nextQuestFlag, questIndex = false, false, 0
 end
 
 function addon:CheckNPC(te)
---@alpha@
 	te = "CkNPC~"..te
---@end-alpha@
 	local npcID = UnitGUID("target") and tonumber( strsub( UnitGUID("target"), -12, -7), 16)
 	if not npcID then
 		npcID = (GossipFrameNpcNameText:GetParent():IsVisible() and GossipFrameNpcNameText:GetText()) or (QuestFrameNpcNameText:GetParent():IsVisible() and QuestFrameNpcNameText:GetText())
 	end
 	if not npcID then
---@alpha@
 		D(te, "no npc/object found")
---@end-alpha@
 		return
 	end
 	local f = false
@@ -477,15 +418,11 @@ function addon:CheckNPC(te)
 		end
 	end
 	if not f then
---@alpha@
 		D(te, "no npc/object found")
---@end-alpha@
 		nextQuestFlag, questIndex = false, 0
 		return
 	else
---@alpha@
 		D(te, "found npc", f)
---@end-alpha@
 		return f
 	end
 end
@@ -496,21 +433,15 @@ local function scrubQuests(title, lvl, triv, ...)
 end
 
 function addon:QuestItteratePickUp(te, ...)
---@alpha@
 	te = "QuPickUp~"..te
---@end-alpha@
 	if (...) == nil then
---@alpha@
 		D(te, "nil opening on the vargArg")
---@end-alpha@
 		return
 	end
 	for i=1, select("#", ...) do
 		local element = select(i, ...)
 		if qTable(element) then
---@alpha@
 			D(te, "found quest, index:", i , "Quest:", element )
---@end-alpha@
 			return i , element
 		end
 	end
@@ -523,63 +454,44 @@ end
 ]]--
 
 function addon:QuestItterateTurnIn(te, ...)
---@alpha@
 	te = "QiTi~"..te
 	D(te, ...)
---@end-alpha@
 	if not (...) then return end
 	local numQuests = select("#", ...)
---@alpha@
 	D(te, "nextQuestFlag:", nextQuestFlag)
 	if nextQuestFlag then	--Means we've been here before and we're moving on...
 		nextQuestFlag = false	--don't want to fk with things
 		questIndex = questIndex +1	--push the index up one so we can move on
---@alpha@
 		D(te, "NextQuest, newIndex:",  questIndex)
---@end-alpha@
 		if questIndex > numQuests then	--if our new index is greater than the available quests, flag it and return the first quest for looping
---@alpha@
 			D(te, "Index is grater than Number, index:", questIndex, "total:", numQuests)
---@end-alpha@
 			npcBad = true		--flag the NPC bad
 			questIndex = 1		--reset the index to 1
---@alpha@
 			D(te, "set flag true and index to 1")
---@end-alpha@
 			for i = 1, numQuests do	--itterate the vars though
 				if qTable(select(i,...)) then	--test quest
 					questIndex = i	--if found set the index
---@alpha@
 					D(te, "found quest", i, (select(i, ...)))
---@end-alpha@
 					return questIndex, (select(i, ...))	--return the index and questName for debugging
 				end
 			end
 		else		--means that we're still in the first round or we havn't hit the end yet
---@alpha@
 			D(te, "index less than total, inner round, push next index for quest")
---@end-alpha@
 			for i = questIndex, numQuests do		--start at our index, as we've allready bumpted it up to the next one in sequence
 				if qTable(select(i,...)) then	--test quest
 					questIndex = i	--if found set the index
---@alpha@
 					D(te, "found quest", i, (select(i, ...)))
---@end-alpha@
 					return questIndex, (select(i, ...))	--return the index and questName for debugging
 				end
 			end
 		end
 	else		-- Weee first quest - this is the likely senerio.
---@alpha@
 		D(te, "First timers")
---@end-alpha@
 		questIndex = 1
 		for i = questIndex, numQuests do		--start at our index, as we've allready bumpted it up to the next one in sequence
 			if qTable(select(i,...)) then	--test quest
 				questIndex = i	--if found set the index
---@alpha@
 				D(te, "found quest", i, (select(i, ...)))
---@end-alpha@
 				return questIndex, (select(i, ...))	--return the index and questName for debugging
 			end
 		end
@@ -587,9 +499,7 @@ function addon:QuestItterateTurnIn(te, ...)
 end
 
 function addon:OpeningCheckQuest(te)
---@alpha@
 	te = "OpQu~"..te
---@end-alpha@
 	local selection, quest = self:QuestItteratePickUp(te, scrubQuests(GetGossipAvailableQuests()))
 	if quest then
 			return selection, quest, "Available"
@@ -602,32 +512,23 @@ function addon:OpeningCheckQuest(te)
 end
 
 function addon:TitleCheck(te)
---@alpha@
 	te = "TitleCk~"..te
 	if qTable(GetTitleText()) then
---@alpha@
 		D(te, GetTitleText())
---@end-alpha@
 		return GetTitleText()
 	end
 end
 
 
 function addon:AnalyzeGossipOptions(te, ...)
---@alpha@
 	te = "EvalGossip~"..te
 	D(te, "evaluating" )
---@end-alpha@
 	local numArgs, count = select("#", ...), 0
 	for i = 1, numArgs, 2 do
 		local element = select(i+1, ...) == "gossip" and select(i, ...) or ""
---@alpha@
 		D(te, "Found element:", element:sub(1,8), (i+1)/2)
---@end-alpha@
 		if gossipOption(element) then
---@alpha@
 			D(te, "Is one of ours")
---@end-alpha@
 			return element, (i+1)/2
 		end
 	end
