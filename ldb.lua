cb555076876ac46493eac723bc8064f06bb7d4b7
OrionShock <OrionShock@gmail.com>
2010-08-02 15:31:04 -0700

    reformat LDB module

--- ldb.lua
+++ ldb.lua
@@ -1,79 +1,39 @@
-local AddonParent = LibStub("AceAddon-3.0"):GetAddon("SickOfClickingDailies")
 
-local D = AddonParent.D
+local AddonName, AddonParent = ...
 
 local module = AddonParent:NewModule("LDB", "AceEvent-3.0")
-module.noModuleControl = true
-local L = LibStub("AceLocale-3.0"):GetLocale("SOCD_Core")
-local db, completedQuests, specialResetQuests, AltTrack
+local L = LibStub("AceLocale-3.0"):GetLocale(AddonName)
 local AceConfigDialog = LibStub("AceConfigDialog-3.0")
-module.sortedQuestTable = {}
+
+
+
+local ldbObj, SecondsToTime, GetQuestResetTime = nil, SecondsToTime, GetQuestResetTime
+local LibDataBroker
 
 function module:OnInitialize()
-	db = AddonParent.db
-	completedQuests = db.char.completedQuests
+	self:Debug("OnInit")
 	LibStub("AceEvent-3.0").RegisterMessage(self, "SOCD_DAILIY_QUEST_COMPLETE")
-	self:PruneHistory()
-	module:SortQuestCompleTable()
-	specialResetQuests = AddonParent.specialResetQuests
-	self:CreateLDB()
-
-	db.RegisterCallback(self, "OnProfileChanged", "RefreshConfig")
-	db.RegisterCallback(self, "OnProfileCopied", "RefreshConfig")
-	db.RegisterCallback(self, "OnProfileReset", "RefreshConfig")
-	self:RegisterEvent("ADDON_LOADED")
+	LibDataBroker = LibStub("LibDataBroker-1.1", true)
 end
 
 function module:OnEnable()
-	AltTrack = AddonParent:GetModule("Alt Tracking", true)
+	self:Debug("OnEnable")
+	self:CreateLDB()
 end
 
-function module:ADDON_LOADED(event, addon, ...)
-	if addon == "SickOfClickingDailies_AltTracking" then
-		AltTrack = AddonParent:GetModule("Alt Tracking", true)
-	end
-end
 
-function module:RefreshConfig(event, f_db, profileName)
-	completedQuests = db.char.completedQuests
-	D(self:GetName(), event, profileName)
-end
-local ldbObj, SecondsToTime, GetQuestResetTime = nil, SecondsToTime, GetQuestResetTime
 
 function module:SOCD_DAILIY_QUEST_COMPLETE(event, quest, opt)
-	D("LDBModule:", event, quest, opt)
-	if specialResetQuests[quest] then
-		completedQuests[quest.." ~ "..date()] = self[ specialResetQuests[quest] ](self)
-	else
-		completedQuests[quest] = time()+ GetQuestResetTime()
-	end
-	if IsInInstance() then
-		print("|cff9933FFSOCD:|r", L["Warning, you are in an instance, DailyQuest Resets Recorded and Time to New Day are not reliable here."])
-	end
-	module:SortQuestCompleTable()
+	self:Debug(event, quest, opt)
 end
 
 local prefix = QUEST_LOG_DAILY_TOOLTIP:match( "\n(.+)" )
 local function OnTooltipShow(self)
 	self:AddDoubleLine( prefix:format( SecondsToTime(GetQuestResetTime()) ),  QUEST_LOG_DAILY_COUNT_TEMPLATE:format(GetDailyQuestsCompleted(), GetMaxDailyQuests())  )
-	if next(completedQuests) and db.char.showExTT then
-		self:AddLine(" ")
-		self:AddDoubleLine(QUESTS_COLON)
-		for i, quest in pairs(module.sortedQuestTable) do
-			self:AddDoubleLine(quest, date("%c", completedQuests[quest]) )
-		end
-	end
 	self:AddLine(" ")
 	self:AddDoubleLine( L["Click: Left for Quest Log"], L["Right for SOCD Options"] )
 end
 
-function module:SortQuestCompleTable()
-	wipe(self.sortedQuestTable)
-	for k, v in pairs(completedQuests) do
-		tinsert( self.sortedQuestTable, k)
-	end
-	table.sort(self.sortedQuestTable)
-end
 
 local function OnEnter(self)
 	GameTooltip:SetOwner(self, "ANCHOR_NONE")
@@ -91,34 +51,40 @@ local function OnClick(frame, button)
 	if button == "LeftButton" then
 		ToggleFrame(QuestLogFrame)
 	elseif button == "RightButton" then
-		if  AceConfigDialog.OpenFrames["SickOfClickingDailies"] then
-			AceConfigDialog:Close("SickOfClickingDailies")
+		if  AceConfigDialog.OpenFrames[AddonName] then
+			AceConfigDialog:Close(AddonName)
 		else
-			AceConfigDialog:Open("SickOfClickingDailies")
+			AceConfigDialog:Open(AddonName)
 		end
 	end
 end
 
 
 function module:CreateLDB()
-	local ldb = LibStub("LibDataBroker-1.1", true)
-	if not ldb then
-		self.ldb = false
-		return
+	if not LibDataBroker then
+		LibDataBroker = LibStub("LibDataBroker-1.1", true)
+		if not LibDataBroker then
+			return
+		end
 	end
 	local dailyTTL = {
 		type = "data source",
 		icon = "Interface\\Icons\\Achievement_Quests_Completed_Daily_08",
 		label = L["Dailies reset in"]..": ",
-		value = SecondsToTime(GetQuestResetTime()) or "~Updating~",
+		value = SecondsToTime(GetQuestResetTime(), true) or "~Updating~",
 		OnEnter = OnEnter,
 		OnLeave = OnLeave,
 		OnTooltipShow = OnTooltipShow,
 		OnClick = OnClick,
 	}
 	dailyTTL.text = (dailyTTL.label)..(dailyTTL.value)
-	self.ldb = ldb:NewDataObject("SOCD - Reset Timer", dailyTTL)
-	ldbObj = self.ldb
+	ldbObj = LibDataBroker:NewDataObject("SOCD - Daily Reset", dailyTTL)
+end
+
+function module:UpdateLDBText()
+	local ttl = GetQuestResetTime()
+	ldbObj.value = SecondsToTime(ttl, true)
+	ldbObj.text = (ldbObj.label)..(ldbObj.value)
 end
 
 local TimerFrame = CreateFrame("frame")
@@ -130,56 +96,16 @@ TimerFrame:SetScript("OnUpdate", function(self, elapsed)
 	delay = delay + elapsed
 	if delay > interval then
 		module:UpdateLDBText()
-		if not timeToEndOfDay then
-			timeToEndOfDay = time() + GetQuestResetTime()
-		end
-		if time() > timeToEndOfDay then
-			module:PruneHistory()
-			if AltTrack then
-				AltTrack:PruneHistory()
-			end
-		end
 		delay = 0
 	end
 end)
 
-
-function module:UpdateLDBText()
-	local ttl = GetQuestResetTime()
-	ldbObj.value = SecondsToTime(ttl)
-	ldbObj.text = (ldbObj.label)..(ldbObj.value)
-end
-
-
-
-function module:PruneHistory()
-	for quest, ttl in pairs(completedQuests) do
-		if time() > ttl then
-			D("Pruning", quest, "Exired on:", date("%c", ttl), "current Time:", date() )
-			completedQuests[quest] = nil
-		end
-	end
-	self:SortQuestCompleTable()
-end
-
-
-function module:GetOptionsTable(rootTable)
-	local ldb_Options = {
-		showExTT = { type = "toggle", name = L["Show Extended Daily Quest Info in LDB Tooltip"], width = "full",
-		get = function(info) return db.char.showExTT end, set = function(info, val) db.char.showExTT = val end, order = 1,
-		},
-	}
-	rootTable.args.MiscOpt.plugins.LDB = ldb_Options
-	return nil
-end
-
 ---------------------------------------------
 -- Reset Functions
 ---------------------------------------------
-do
-	function module:Exclude()
-		return nil
-	end
+
+function module:Exclude()
+	return nil
 end
 
 
@@ -206,7 +132,6 @@ do		-- === Wintergrasp Reset Function ===
 
 	local diff = {}
 	function module:GetNextWGReset()
-
 		local cur_day, cur_month, cur_year = tonumber(date("%d")), tonumber(date("%m")), tonumber(date("%Y"))
 		local cur_wDay = tonumber(date("%w")) + 1
 		for k, _ in pairs(diff) do diff[k] = nil end
@@ -235,5 +160,3 @@ do		-- === Wintergrasp Reset Function ===
 		return time(diff)
 	end
 end
-
-
