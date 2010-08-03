fa0065772cc25bb572a6f703b5899f531504bbd4
OrionShock <OrionShock@gmail.com>
2010-08-03 01:24:44 -0700

    aparently adding one line means that all the others changed too...

--- wotlk.lua
+++ wotlk.lua
@@ -7,6 +7,7 @@ local AddonName, AddonParent = ...
 
 local module = AddonParent:NewModule("LK")
 local L = LibStub("AceLocale-3.0"):GetLocale(AddonName)
+local db
 --local GT = LibStub("AceLocale-3.0"):GetLocale("SOCD_GossipText")
 
 function module:OnInitialize()
@@ -37,6 +38,7 @@ function module:OnEnable()
 
 	SetItemRef("item:45724", "item:45724")	--Champion's Purse
 	SetItemRef("item:45724", "item:45724")
+	db = AddonParent.db
 end
 
 function module:OnDisable()
@@ -68,7 +70,7 @@ function module:SOCD_QuestByID_Ready(event, ...)
 		get = "QuestOptGet",
 		set = "QuestOptSet",
 		args = {
-			tournyPre = { name = L["Argent Crusade"], type = "group",
+			tournyPre = { name = L["Argent Crusade"], type = "group", inline = true,
 				args = {
 					["BattleBeforeTheCitadel"] = GetOptionGroup( 13861 , champQuestRewardOpts ),	--"Battle Before The Citadel"
 					["ThreatFromAbove"] = GetOptionGroup( 13682 , champQuestRewardOpts ),	--"Threat From Above"
@@ -76,7 +78,7 @@ function module:SOCD_QuestByID_Ready(event, ...)
 					["TakingBattleToTheEnemy"] = GetOptionGroup( 13789 , champQuestRewardOpts ),	--"Taking Battle To The Enemy"
 				},
 			},
-			thanksgiving = { name = "Thankgsgiving Quests", type = "group",
+			thanksgiving = { name = "Thanksgiving Quests", type = "group", inline = true,
 				args = {
 					["CantGetEnoughTurkey"] = GetOptionGroup( 14061 , thxgivingRewardOpts ),	--"Can't Get Enough Turkey"
 					["DontForgetTheStuffing!"] = GetOptionGroup( 14062 , thxgivingRewardOpts ),	--"Don't Forget The Stuffing!"
@@ -92,41 +94,41 @@ function module:SOCD_QuestByID_Ready(event, ...)
 	---Special Reset Quests :)
 	local moduleSpecialQuests = {
 		--Wintergraps PvP Quests
-		[GetLocalizedQuestNameByID(13195)] = "GetNextWGReset"	--"A Rare Herb"
-		[GetLocalizedQuestNameByID(13199)] = "GetNextWGReset"	--"Bones and Arrows"
-		[GetLocalizedQuestNameByID(13222)] = "GetNextWGReset"	--"Defend the Siege"
-		[GetLocalizedQuestNameByID(13191)] = "GetNextWGReset"	--"Fueling the Demolishers"
-		[GetLocalizedQuestNameByID(13201)] = "GetNextWGReset"	--"Healing with Roses"
-		[GetLocalizedQuestNameByID(13202)] = "GetNextWGReset"	--"Jinxing the Walls"
-		[GetLocalizedQuestNameByID(13177)] = "GetNextWGReset"	--"No Mercy for the Merciless"
-		[GetLocalizedQuestNameByID(13178)] = "GetNextWGReset"	--"Slay them all!"
-		[GetLocalizedQuestNameByID(13186)] = "GetNextWGReset"	--"Stop the Siege"
-		[GetLocalizedQuestNameByID(13181)] = "GetNextWGReset"	--"Victory in Wintergrasp"
-		[GetLocalizedQuestNameByID(13192)] = "GetNextWGReset"	--"Warding the Walls"
-		[GetLocalizedQuestNameByID(13198)] = "GetNextWGReset"	--"Warding the Warriors"
-		[GetLocalizedQuestNameByID(13538)] = "GetNextWGReset"	--"Southern Sabotage"
-		[GetLocalizedQuestNameByID(13539)] = "GetNextWGReset"	--"Toppling the Towers"
+		[GetLocalizedQuestNameByID(13195)] = "GetNextWGReset",	--"A Rare Herb"
+		[GetLocalizedQuestNameByID(13199)] = "GetNextWGReset",	--"Bones and Arrows"
+		[GetLocalizedQuestNameByID(13222)] = "GetNextWGReset",	--"Defend the Siege"
+		[GetLocalizedQuestNameByID(13191)] = "GetNextWGReset",	--"Fueling the Demolishers"
+		[GetLocalizedQuestNameByID(13201)] = "GetNextWGReset",	--"Healing with Roses"
+		[GetLocalizedQuestNameByID(13202)] = "GetNextWGReset",	--"Jinxing the Walls"
+		[GetLocalizedQuestNameByID(13177)] = "GetNextWGReset",	--"No Mercy for the Merciless"
+		[GetLocalizedQuestNameByID(13178)] = "GetNextWGReset",	--"Slay them all!"
+		[GetLocalizedQuestNameByID(13186)] = "GetNextWGReset",	--"Stop the Siege"
+		[GetLocalizedQuestNameByID(13181)] = "GetNextWGReset",	--"Victory in Wintergrasp"
+		[GetLocalizedQuestNameByID(13192)] = "GetNextWGReset",	--"Warding the Walls"
+		[GetLocalizedQuestNameByID(13198)] = "GetNextWGReset",	--"Warding the Warriors"
+		[GetLocalizedQuestNameByID(13538)] = "GetNextWGReset",	--"Southern Sabotage"
+		[GetLocalizedQuestNameByID(13539)] = "GetNextWGReset",	--"Toppling the Towers"
 
 		--Raid Weekly Quests
-		[GetLocalizedQuestNameByID(24580)] = "GetNextWGReset"	--"Anub'Rekhan Must Die!"
-		[GetLocalizedQuestNameByID(24585)] = "GetNextWGReset"	--"Flame Leviathan Must Die!"
-		[GetLocalizedQuestNameByID(24587)] = "GetNextWGReset"	--"Ignis the Furnace Master Must Die!"
-		[GetLocalizedQuestNameByID(24582)] = "GetNextWGReset"	--"Instructor Razuvious Must Die!"
-		[GetLocalizedQuestNameByID(24589)] = "GetNextWGReset"	--"Lord Jaraxxus Must Die!"
-		[GetLocalizedQuestNameByID(24590)] = "GetNextWGReset"	--"Lord Marrowgar Must Die!"
-		[GetLocalizedQuestNameByID(24584)] = "GetNextWGReset"	--"Malygos Must Die!"
-		[GetLocalizedQuestNameByID(24581)] = "GetNextWGReset"	--"Noth the Plaguebringer Must Die!"
-		[GetLocalizedQuestNameByID(24583)] = "GetNextWGReset"	--"Patchwerk Must Die!"
-		[GetLocalizedQuestNameByID(24586)] = "GetNextWGReset"	--"Razorscale Must Die!"
-		[GetLocalizedQuestNameByID(24579)] = "GetNextWGReset"	--"Sartharion Must Die!"
-		[GetLocalizedQuestNameByID(24588)] = "GetNextWGReset"	--"XT-002 Deconstructor Must Die!"
+		[GetLocalizedQuestNameByID(24580)] = "GetNextWGReset",	--"Anub'Rekhan Must Die!"
+		[GetLocalizedQuestNameByID(24585)] = "GetNextWGReset",	--"Flame Leviathan Must Die!"
+		[GetLocalizedQuestNameByID(24587)] = "GetNextWGReset",	--"Ignis the Furnace Master Must Die!"
+		[GetLocalizedQuestNameByID(24582)] = "GetNextWGReset",	--"Instructor Razuvious Must Die!"
+		[GetLocalizedQuestNameByID(24589)] = "GetNextWGReset",	--"Lord Jaraxxus Must Die!"
+		[GetLocalizedQuestNameByID(24590)] = "GetNextWGReset",	--"Lord Marrowgar Must Die!"
+		[GetLocalizedQuestNameByID(24584)] = "GetNextWGReset",	--"Malygos Must Die!"
+		[GetLocalizedQuestNameByID(24581)] = "GetNextWGReset",	--"Noth the Plaguebringer Must Die!"
+		[GetLocalizedQuestNameByID(24583)] = "GetNextWGReset",	--"Patchwerk Must Die!"
+		[GetLocalizedQuestNameByID(24586)] = "GetNextWGReset",	--"Razorscale Must Die!"
+		[GetLocalizedQuestNameByID(24579)] = "GetNextWGReset",	--"Sartharion Must Die!"
+		[GetLocalizedQuestNameByID(24588)] = "GetNextWGReset",	--"XT-002 Deconstructor Must Die!"
 
 		--ICC Weekly Quests
-		[GetLocalizedQuestNameByID(24879)] = "GetNextWGReset"	--"Blood Quickening"
-		[GetLocalizedQuestNameByID(24875)] = "GetNextWGReset"	--"Deprogramming"
-		[GetLocalizedQuestNameByID(24878)] = "GetNextWGReset"	--"Residue Rendezvous"
-		[GetLocalizedQuestNameByID(24880)] = "GetNextWGReset"	--"Respite for a Tormented Soul"
-		[GetLocalizedQuestNameByID(24877)] = "GetNextWGReset"	--"Securing the Ramparts"
+		[GetLocalizedQuestNameByID(24879)] = "GetNextWGReset",	--"Blood Quickening"
+		[GetLocalizedQuestNameByID(24875)] = "GetNextWGReset",	--"Deprogramming"
+		[GetLocalizedQuestNameByID(24878)] = "GetNextWGReset",	--"Residue Rendezvous"
+		[GetLocalizedQuestNameByID(24880)] = "GetNextWGReset",	--"Respite for a Tormented Soul"
+		[GetLocalizedQuestNameByID(24877)] = "GetNextWGReset",	--"Securing the Ramparts"
 
 	}
 	for k,v in pairs(moduleSpecialQuests) do
