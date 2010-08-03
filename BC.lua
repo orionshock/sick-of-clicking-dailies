c5b23afdf1d047de858b761849bf97906f126438
OrionShock <OrionShock@gmail.com>
2010-08-02 23:56:44 -0700

    Make options table work properly.

--- BC.lua
+++ BC.lua
@@ -11,6 +11,7 @@
 local AddonName, AddonParent = ...
 local module = AddonParent:NewModule("BC")
 local L = LibStub("AceLocale-3.0"):GetLocale(AddonName)
+local db
 --local GT = LibStub("AceLocale-3.0"):GetLocale("SOCD_GossipText")
 local GetLocalizedQuestNameByID = AddonParent.GetLocalizedQuestNameByID
 
@@ -40,7 +41,7 @@ function module:OnEnable()
 	SetItemRef("item:33857","item:33857")	--Crate Of Meat
 	SetItemRef("item:33857","item:33857")
 
-
+	db = AddonParent.db
 end
 
 function module:OnDisable()
@@ -57,7 +58,8 @@ local function GetOptionGroup(id, ...)
 end
 
 function module:SOCD_QuestByID_Ready(event, ...)
-	self:Debug(event, ...)
+	--self:Debug(event, ...)
+	--Create Options Table
 	local opts = {
 		name = L["Burning Crusade"],
 		type = "group",
@@ -87,8 +89,9 @@ function module:SOCD_QuestByID_Ready(event, ...)
 --			}.
 		},
 	}
-	self.options = opts
+	self.options = opts	--Set it onto the main of the module, it'll be picked up later.
 
+	--Handle the special quests we do and exclude them.
 	local moduleSpecialQuests = {
 		[ L["Candy Bucket"] ] = "Exclude",
 	}
@@ -96,22 +99,47 @@ function module:SOCD_QuestByID_Ready(event, ...)
 		AddonParent.SpecialQuestResets[k] = v
 	end
 
+
 	--Quests That need to be dsabled by default.
+	local dbLoc = AddonParent.db.profile.QuestStatus	--Cache the DB location on this.
 	local tempTitle = GetLocalizedQuestNameByID(11545)	--"A Charitable Donation"
-	AddonParent.db.profile.QuestStatus[ tempTitle ] = AddonParent.db.profile.QuestStatus[ tempTitle ] or false
+	dbLoc[ tempTitle ] = dbLoc[ tempTitle ] or false
 
 	tempTitle = GetLocalizedQuestNameByID(11548)	--"Your Continued Support"
-	AddonParent.db.profile.QuestStatus[ tempTitle ] = AddonParent.db.profile.QuestStatus[ tempTitle ] or false
+	dbLoc[ tempTitle ] = dbLoc[ tempTitle ] or false
 
+	--Quest with reward options that need to be defaulted.
+	dbLoc = AddonParent.db.profile.QuestRewardOptions	--Recache the new location for the next section
+
+	tempTitle = GetLocalizedQuestNameByID(11379)	--SuperHotStew
+	dbLoc[tempTitle] = dbLoc[tempTitle] or -1
+
+	tempTitle = GetLocalizedQuestNameByID(11381)	--SoupForTheSoul
+	dbLoc[tempTitle] = dbLoc[tempTitle] or -1
+
+	tempTitle = GetLocalizedQuestNameByID(11377)	--RevengeIsTasty
+	dbLoc[tempTitle] = dbLoc[tempTitle] or -1
+
+	tempTitle = GetLocalizedQuestNameByID(11380)	--Manalicious
+	dbLoc[tempTitle] = dbLoc[tempTitle] or -1
+
+	tempTitle = GetLocalizedQuestNameByID(11515)	--BloodForBlood
+	dbLoc[tempTitle] = dbLoc[tempTitle] or -1
+
+	tempTitle = GetLocalizedQuestNameByID(11544)	--AtamalArmaments
+	dbLoc[tempTitle] = dbLoc[tempTitle] or -1
 
 end
 
 function module:QuestOptGet(info, ...)
-	print("QuestOptGet")
+	--self:Debug("QuestOptGet", db.profile.QuestRewardOptions[ info.option.name ] )
+	return db.profile.QuestRewardOptions[ info.option.name ] 
 end
 
-function module:QuestOptSet(info, ...)
-	print("QuestOptSet")
+function module:QuestOptSet(info, value, ...)
+	--self:Debug("QuestOptSet", info.option.name, value)
+	db.profile.QuestRewardOptions[ info.option.name ]  = value
+
 end
 
 --Gossip Text to skipp though.. Will use later.
