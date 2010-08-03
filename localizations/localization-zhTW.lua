aec291fe524b1a148eb0906d77ec4bb3752f862c
OrionShock <OrionShock@gmail.com>
2010-08-03 12:32:00 -0700

    update locale files

--- localizations/localization-zhTW.lua
+++ localizations/localization-zhTW.lua
@@ -5,108 +5,45 @@
 	Please Visit: http://www.wowace.com/addons/sick-of-clicking-dailies/pages/how-to-add-localizations/
 	to contribute to localizations :)
 ]]--
+local addonName = ...
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
 
+local genderMale = UnitSex("player") == 2
 
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "zhTW", true, debug)
 
-
-
-
-
-----------------------------------------------------------------------------
---	General -- Used by all or most :)				  --
-----------------------------------------------------------------------------
-
-local L = LibStub("AceLocale-3.0"):NewLocale("SOCD_Core", "zhTW", false)
-
-	if L then
---These are not importable to WoWAce/Curseforge Locale system, as these are pulled directly from the client.
-L["Alliance"] = FACTION_ALLIANCE
-L["Battlegrounds"] = BATTLEFIELDS
-L["Cooking"] = (GetSpellInfo(2550))
-L["Dungeon"] = LFG_TYPE_DUNGEON
-L["Doungeons"] = BUG_CATEGORY3
-L["Faction"] = FACTION
-L["Fishing"] = (GetSpellInfo(63275))
-L["Friendly"] = FACTION_STANDING_LABEL5
-L["Heroic Dungeon"] = LFG_TYPE_HEROIC_DUNGEON
-L["Honored"] = FACTION_STANDING_LABEL6
-L["Horde"] = FACTION_HORDE
-L["Jewelcrafting"] = (GetSpellInfo(25229))
-L["None"] = LFG_TYPE_NONE
-L["Netural"] = FACTION_STANDING_LABEL4
-L["Professions"] = TRADE_SKILLS
-L["PvP"] = PVP
-L["Quests"] = QUESTS_LABEL
-L["Revered"] = FACTION_STANDING_LABEL7
-L["Wintergrasp"] = PVPBATTLEGROUND_WINTERGRASPTIMER:match("(.+)|n")
+if L then
 
 --@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="none", table-name="L")@
 
-	end	
+end
 
---All Quests Classified by orgin / content location
-
-----------------------------------------------------------------------------
---	Classic, Populated by WoWAce-Curseforge Packager.		  --
-----------------------------------------------------------------------------
-
-local C = LibStub("AceLocale-3.0"):NewLocale("SOCD_Classic", "zhTW", false)
-
-	if C then
-
---@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, namespace="Classic", table-name="C")@
-
-	end
-
-----------------------------------------------------------------------------
---	TBC, Populated by WoWAce-Curseforge Packager.			  --
-----------------------------------------------------------------------------	
-
-local BC = LibStub("AceLocale-3.0"):NewLocale("SOCD_BC", "zhTW", false)
-
-	if BC then
-
---@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, namespace="BC", table-name="BC")@
-
-	end
-
-----------------------------------------------------------------------------
---	WOTLK, Populated by WoWAce-Curseforge Packager.			  --
-----------------------------------------------------------------------------
-
-local LK = LibStub("AceLocale-3.0"):NewLocale("SOCD_LK", "zhTW", false)
-
-	if LK then
-
---@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, namespace="Wrath", table-name="LK")@
-
-	end
-
-----------------------------------------------------------------------------
---	Gossip Texts, Populated by WoWAce-Curseforge Packager.			  --
-----------------------------------------------------------------------------
-
-local GT = LibStub("AceLocale-3.0"):NewLocale("SOCD_GossipText", "zhTW", false)
+local GT = LibStub("AceLocale-3.0"):NewLocale(addonName.."GossipText", "zhTW", true, debug)
 	if GT then
 
---@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, namespace="GossipTexts", table-name="GT")@
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, namespace="GossipText", table-name="GT")@
 
 
-		local GT_R = LibStub("AceLocale-3.0"):GetLocale("SOCD_GossipText")
-		if genderMale then	--Masculine Context
+	local GT_R = LibStub("AceLocale-3.0"):GetLocale(addonName.."GossipText")
+	if genderMale then	--Masculine Context
 
-GT["I'm ready to work for you today!  Give me the good stuff!"] = GT_R["I'm ready to work for you today!  Give me the good stuff!(M)"]:gsub("[%(MF%)]+$", "")
-GT["I'm ready to work for you today!  Give me that ram!"] = GT_R["I'm ready to work for you today!  Give me that ram!(M)"]:gsub("[%(MF%)]+$", "")
-GT["I'm ready to begin. What is the first ingredient you require?"] = GT_R["I'm ready to begin. What is the first ingredient you require?(M)"]:gsub("[%(MF%)]+$", "")
-GT["I am ready to fight!"] = GT_R["I am ready to fight!(M)"]:gsub("[%(MF%)]+$", "")
+		GT["I'm ready to work for you today!  Give me the good stuff!"] = GT_R["I'm ready to work for you today!  Give me the good stuff!(M)"]:gsub("[%(MF%)]+$", "")
+		GT["I'm ready to work for you today!  Give me that ram!"] = GT_R["I'm ready to work for you today!  Give me that ram!(M)"]:gsub("[%(MF%)]+$", "")
+		GT["I'm ready to begin. What is the first ingredient you require?"] = GT_R["I'm ready to begin. What is the first ingredient you require?(M)"]:gsub("[%(MF%)]+$", "")
+		GT["I am ready to fight!"] = GT_R["I am ready to fight!(M)"]:gsub("[%(MF%)]+$", "")
 
-		else			--Feminine Context
+	else			--Feminine Context
 
-GT["I'm ready to work for you today!  Give me the good stuff!"] = GT_R["I'm ready to work for you today!  Give me the good stuff!(F)"]:gsub("[%(MF%)]+$", "")
-GT["I'm ready to work for you today!  Give me that ram!"] = GT_R["I'm ready to work for you today!  Give me that ram!(F)"]:gsub("[%(MF%)]+$", "")
-GT["I'm ready to begin. What is the first ingredient you require?"] = GT_R["I'm ready to begin. What is the first ingredient you require?(F)"]:gsub("[%(MF%)]+$", "")
-GT["I am ready to fight!"] = GT_R["I am ready to fight!(F)"]:gsub("[%(MF%)]+$", "")
-		end
+		GT["I'm ready to work for you today!  Give me the good stuff!"] = GT_R["I'm ready to work for you today!  Give me the good stuff!(F)"]:gsub("[%(MF%)]+$", "")
+		GT["I'm ready to work for you today!  Give me that ram!"] = GT_R["I'm ready to work for you today!  Give me that ram!(F)"]:gsub("[%(MF%)]+$", "")
+		GT["I'm ready to begin. What is the first ingredient you require?"] = GT_R["I'm ready to begin. What is the first ingredient you require?(F)"]:gsub("[%(MF%)]+$", "")
+		GT["I am ready to fight!"] = GT_R["I am ready to fight!(F)"]:gsub("[%(MF%)]+$", "")
 	end
+end
 
 
+
+---Localization Counter-- Bump to generate new zip for locale changes = 16
